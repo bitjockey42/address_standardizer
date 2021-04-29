d21d484ed1ff03e33d39f775de9598913caf05ab
Al Julian <github@ajulian.me>
2021-04-28 22:33:16 -0500

    test for designators

--- tests/test_abbreviator.py
+++ tests/test_abbreviator.py
@@ -5,3 +5,5 @@ from address_standardizer.abbreviator import abbreviate
 def test_abbreviate():
     assert abbreviate("alley") == "ALY"
     assert abbreviate("aPartMent") == "APT"
+    assert abbreviate("SPACE") == "SPC"
+    assert abbreviate("front") == "FRNT"
