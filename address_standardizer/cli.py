64c2e0d5c3e55b565b8206657e2ac4737ab05244
Al Julian <github@ajulian.me>
2021-04-28 21:42:49 -0500

    Setup the cli

--- address_standardizer/cli.py
+++ address_standardizer/cli.py
@@ -4,15 +4,17 @@ import sys
 import click
 
 
-@click.command()
-def main():
+@click.group()
+def main(args=None):
     """Console script for address_standardizer."""
-    click.echo(
-        "Replace this message by putting your code into "
-        "address_standardizer.cli.main"
-    )
-    click.echo("See click documentation at https://click.palletsprojects.com/")
     return 0
+
+
+@main.command()
+@click.argument("filename")
+def update(filename):
+    """Generate or update the lookup map"""
+    click.echo("Update")
 
 
 if __name__ == "__main__":
