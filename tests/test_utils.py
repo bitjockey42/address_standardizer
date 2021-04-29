490ddc8227f777cc0901020b7ba0a8471b30b63f
Al Julian <github@ajulian.me>
2021-04-28 21:27:51 -0500

    Test process csv

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -5,6 +5,14 @@ from address_standardizer import utils
 def test_process_csv(mocker):
     mocker.patch(
         "address_standardizer.utils.read_csv",
-        return_value=[{"primary": "ALLEY", "standard": "ALY"}],
+        return_value=[
+            {"primary": "ALLEY", "standard": "ALY", "common": "ALLEY"},
+            {"primary": "", "standard": "", "common": "ALLY"},
+            {"primary": "ANEX", "standard": "ANX", "common": "ANNEX"},
+        ],
     )
-    assert utils.process_csv("test.csv") == [{"primary": "ALLEY", "standard": "ALY"}]
+    assert utils.process_csv("test.csv") == [
+        {"primary": "ALLEY", "standard": "ALY", "common": "ALLEY"},
+        {"primary": "ALLEY", "standard": "ALY", "common": "ALLY"},
+        {"primary": "ANEX", "standard": "ANX", "common": "ANNEX"},
+    ]
