61d95702508db6dd642d1bd7ab6253a412df02dc
Al Julian <github@ajulian.me>
2021-04-28 23:58:25 -0500

    Remove special characters

--- address_standardizer/standardizer.py
+++ address_standardizer/standardizer.py
@@ -1,4 +1,6 @@
 """Main module."""
+import re
+
 from address_standardizer.abbreviator import abbreviate
 
 
@@ -6,6 +8,9 @@ def standardize(address):
     parts = address.upper().strip().split(" ")
 
     for i, part in enumerate(parts):
+        # Remove special characters
+        part = re.sub(r"[^a-zA-Z0-9]+", "", part)
+
         if abbreviate(part) is None:
             continue
 
--- tests/test_standardizer.py
+++ tests/test_standardizer.py
@@ -7,7 +7,8 @@ from address_standardizer.standardizer import standardize
 @pytest.mark.parametrize(
     "address,expected",
     [
-        ("1234 Main Street", "1234 MAIN ST")
+        ("1234 Main Street", "1234 MAIN ST"),
+        ("1234 Main St.", "1234 MAIN ST"),
     ],
 )
 def test_standardize(address, expected):
