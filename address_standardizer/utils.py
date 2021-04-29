84cc5fffefcccf363538d1c79e822be396b81297
Al Julian <github@ajulian.me>
2021-04-28 21:03:38 -0500

    Add util to process address standard csv

--- address_standardizer/utils.py
+++ address_standardizer/utils.py
@@ -5,3 +5,18 @@ def read_csv(filename):
     with open(filename, newline="") as csv_file:
         reader = csv.DictReader(csv_file)
         return [row for row in reader]
+
+
+def process_csv(filename):
+    rows = read_csv(filename)
+
+    for row in rows:
+        if row["primary"] and row["standard"]:
+            primary = row["primary"]
+            standard = row["standard"]
+
+        if not row["primary"] and not row["standard"]:
+            row["primary"] = primary
+            row["standard"] = standard
+
+    return rows
