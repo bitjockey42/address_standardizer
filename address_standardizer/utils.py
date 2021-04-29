67cf79368387a0f9c07ee45b04fba05fb6fc9bd8
Al Julian <github@ajulian.me>
2021-04-28 21:35:26 -0500

    Generate a lookup map

--- address_standardizer/utils.py
+++ address_standardizer/utils.py
@@ -1,4 +1,5 @@
 import csv
+import json
 
 from collections import defaultdict
 
@@ -7,6 +8,11 @@ def read_csv(filename):
     with open(filename, newline="") as csv_file:
         reader = csv.DictReader(csv_file)
         return [row for row in reader]
+
+
+def write_json(data, filename):
+    with open(filename, "w+") as json_file:
+        json.dump(data, json_file)
 
 
 def process_csv(filename):
@@ -32,3 +38,13 @@ def generate_standards_map(filename):
         standards_map[row["standard"]].append(row["common"])
 
     return standards_map
+
+
+def generate_lookup_map(filename):
+    rows = process_csv(filename)
+    lookup_map = defaultdict(str)
+
+    for row in rows:
+        lookup_map[row["common"]] = row["standard"]
+
+    return lookup_map
